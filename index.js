e4b48c8a754b34fce2d7411d176619d80a9bec6e
Mila <milaz3388@gmail.com>
2026-04-13 22:35:57 +0800

    fail: test should fail

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // 2. 定義路由 (Route) 與 邏輯
 app.get('/', (req, res) => {
-  res.status(200).send('Hello CI/CD!');
+  res.status(200).send('Hello World!');
 });
 
 // 3. 匯出模組 (為了讓測試工具能抓到它)
